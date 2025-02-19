9b11e5a6ad98555b8d98fa1757e395e11875a72d
Jefferson <ifrit68@hotmail.com>
2025-02-18 23:06:12 -0500

    FEATURE[new things]

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,31 +5,14 @@ import { defineConfig, envField } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
+import solidJs from '@astrojs/solid-js';
 
-  env: {
-    schema: {
-      SPOTIFY_CLIENT_ID: envField.string({
-        context: 'server',
-        access: 'secret',
-        default: '',
-      }),
-      SPOTIFY_CLIENT_SECRET: envField.string({
-        context: 'server',
-        access: 'secret',
-        default: '',
-      }),
-      SPOTIFY_REDIRECT_URI: envField.string({
-        context: 'server',
-        access: 'secret',
-        default: '',
-      }),
-    },
-  },
+import alpinejs from '@astrojs/alpinejs';
+
+export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [solidJs(), alpinejs()],
 });
