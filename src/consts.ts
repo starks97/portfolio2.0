671a686d6289a0b047ba0daa5f7855f69ddb1dc9
Jefferson <ifrit68@hotmail.com>
2025-11-14 12:20:07 -0500

    maintainence[update me.webp and resume & update libraries]

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,1 @@
-export const TOKEN = 'spotify_access_token';
-export const SCOPE = 'user-read-private user-read-email';
-export const SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize?';
-export const SPOTIFY_REQUEST_NEW_TOKEN_URL =
-  'https://accounts.spotify.com/api/token';
-export const SPOTIFY_REQUEST_PLAYLIST_URL =
-  'https://api.spotify.com/v1/users/25xn3lx8ghfoi9gtycg9nn70t/playlists';
-
-export const SPOTIFY_REQUEST_PLAYLIST_ITEMS_URL =
-  'https://api.spotify.com/v1/playlists/';
-
-export const SPOTIFY_REQUEST_ALBUMS_URL = 'https://api.spotify.com/v1/albums';
-
-export const SPOTIFY_REQUEST_SAVED_TRACKS_URL =
-  'https://api.spotify.com/v1/me/tracks';
-
 export const EMAIL = 'jeffersonespinoza45@gmail.com';
